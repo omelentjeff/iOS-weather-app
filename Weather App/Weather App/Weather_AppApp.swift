23bcb5cbcdbf3f7fc993fd07fdba9e53e70bf2a8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-04 16:40:28 +0300

    feat: create project

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Otto Melentjeff on 4.5.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
