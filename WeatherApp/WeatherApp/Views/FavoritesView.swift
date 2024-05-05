3d7bfaf61a4d2c735a31cde3ca24123e7e71c723
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 18:03:56 +0300

    feat: style the view

--- WeatherApp/WeatherApp/Views/FavoritesView.swift
+++ WeatherApp/WeatherApp/Views/FavoritesView.swift
@@ -11,6 +11,19 @@ struct FavoritesView: View {
     @Binding var isFavoritesActive: Bool
     
     var body: some View {
-        Text("Favorites")
+        VStack {
+            Text("Favorites").padding(.top, 20).font(.system(size: 24)).bold()
+            HStack {
+                Button(action: { isFavoritesActive = false}) {
+                    Text("Close")
+                        .padding()
+                        .font(.system(size: 10))
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+            }.padding(.top, 20)
+            Spacer()
+        }
     }
 }
