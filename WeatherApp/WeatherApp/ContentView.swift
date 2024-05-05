8bb6b589ef0e303879aa4f94f3033b09cff9cce5
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 15:24:47 +0300

    create project

---
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 5.5.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 5.5.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
