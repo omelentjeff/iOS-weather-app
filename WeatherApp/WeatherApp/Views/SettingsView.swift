0060486a79e68e38051cb96c95a017ddfb193701
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 17:21:41 +0300

    feat: create SettingsView template

--- WeatherApp/WeatherApp/Views/SettingsView.swift
+++ WeatherApp/WeatherApp/Views/SettingsView.swift
@@ -8,11 +8,8 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Binding var isSettingsActive: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Settings")
     }
-}
-
-#Preview {
-    SettingsView()
 }
