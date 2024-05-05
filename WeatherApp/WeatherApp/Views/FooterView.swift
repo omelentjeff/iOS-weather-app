995702b0c49b2138e9880ef44f004477e7a4b8d6
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 15:52:13 +0300

    feat: create MyButton and implement footer

--- WeatherApp/WeatherApp/Views/FooterView.swift
+++ WeatherApp/WeatherApp/Views/FooterView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct FooterView: View {
     var body: some View {
         HStack {
-            MyButton(image: "gearshape", color: Color.white, action: {})
+            MyButton(image: "gearshape", color: Color.white, label: "settings", action: {})
             Spacer()
-            MyButton(image: "magnifyingglass", color: Color.white, action: {})
+            MyButton(image: "magnifyingglass", color: Color.white, label: "search", action: {})
         }.background(.blue)
     }
 }
@@ -20,15 +20,19 @@ struct FooterView: View {
 struct MyButton: View {
     var image: String
     var color: Color
+    var label: String
     var action: () -> Void
     
     var body: some View {
-        Button(action: action, label: {
-            Image(systemName: image)
-                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing:20))
+        VStack {
+            Button(action: action, label: {
+                Image(systemName: image)
+                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 1, trailing: 20))
                     .font(.system(size: 24))
                     .foregroundStyle(color)
-                    //.cornerRadius(20)
-                })
+                //.cornerRadius(20)
+            })
+            Text(label).foregroundColor(.white).font(.system(size: 12)).padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
     }
 }
