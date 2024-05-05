959f7a92a12c9146420efd7e086c17f6021af0e6
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 17:09:28 +0300

    feat: refactor MyButton to own file

--- WeatherApp/WeatherApp/Views/MyButton.swift
+++ WeatherApp/WeatherApp/Views/MyButton.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct MyButton: View {
+    var image: String
+    var color: Color
+    var label: String
+    var textColor: Color
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: action, label: {
+                Image(systemName: image)
+                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 1, trailing: 20))
+                    .font(.system(size: 24))
+                    .foregroundStyle(color)
+                //.cornerRadius(20)
+            })
+            Text(label).foregroundColor(textColor).font(.system(size: 12)).padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
     }
-}
-
-#Preview {
-    MyButton()
 }
