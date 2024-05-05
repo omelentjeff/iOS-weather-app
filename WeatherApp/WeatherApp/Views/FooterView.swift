4fc934295c09c78e9b3d6fe62bd13db503560c9d
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 17:22:04 +0300

    feat: implement navigation for footer buttons

--- WeatherApp/WeatherApp/Views/FooterView.swift
+++ WeatherApp/WeatherApp/Views/FooterView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct FooterView: View {
     @State var isSearchActive: Bool = false
+    @State var isSettingsActive: Bool = false
+    @State var isFavoritesActive: Bool = false
+
     
     var body: some View {
         HStack {
-            MyButton(image: "star", color: Color.white, label: "Favorites", textColor: Color.white, action: {})
+            MyButton(image: "star", color: Color.white, label: "Favorites", textColor: Color.white, action: {isFavoritesActive = true}).sheet(isPresented: $isFavoritesActive, content: {
+                FavoritesView(isFavoritesActive: $isFavoritesActive)
+            })
             Spacer()
             MyButton(image: "magnifyingglass", color: Color.white, label: "Search", textColor: Color.white, action: {isSearchActive = true}).sheet(isPresented: $isSearchActive, content: {
                 SearchView(isSearchActive: $isSearchActive)
             })
             Spacer()
-            MyButton(image: "gearshape", color: Color.white, label: "Settings", textColor: Color.white, action: {})
+            MyButton(image: "gearshape", color: Color.white, label: "Settings", textColor: Color.white, action: {isSettingsActive = true}).sheet(isPresented: $isSettingsActive, content: {
+                SettingsView(isSettingsActive: $isSettingsActive)
+            })
         }.background(.blue)
     }
 }
