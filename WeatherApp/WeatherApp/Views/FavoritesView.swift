dbd82ca42cd075539cee947861c6bde08580c04d
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-05 17:21:18 +0300

    feat: create FavoritesView template

---
+++ WeatherApp/WeatherApp/Views/FavoritesView.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 5.5.2024.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    @Binding var isFavoritesActive: Bool
+    
+    var body: some View {
+        Text("Favorites")
+    }
+}
