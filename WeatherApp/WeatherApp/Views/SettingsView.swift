de10b31aaee3f8b213df401f913eb3d2aa116615
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-06 13:41:03 +0300

    feat: add dark mode toggle in SettingsView

--- WeatherApp/WeatherApp/Views/SettingsView.swift
+++ WeatherApp/WeatherApp/Views/SettingsView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var darkMode = true
     var body: some View {
         VStack {
             Text("Settings").padding(.top, 20).font(.system(size: 24)).bold()
+            Toggle("Dark Mode", isOn: $darkMode).padding(60)
             Spacer()
         }
     }
