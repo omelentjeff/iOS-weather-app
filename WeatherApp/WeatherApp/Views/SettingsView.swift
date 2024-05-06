b0b8f8046668a07bf5e705012eaf8900c30198fc
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-06 18:14:26 +0300

    feat: implement dark mode in SettingsView

--- WeatherApp/WeatherApp/Views/SettingsView.swift
+++ WeatherApp/WeatherApp/Views/SettingsView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var darkMode = true
+    @State private var toggleOn: Bool = false
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
+    
     var body: some View {
         VStack {
             Text("Settings").padding(.top, 20).font(.system(size: 24)).bold()
-            Toggle("Dark Mode", isOn: $darkMode).padding(60)
+            Spacer()
+            
+            HStack {
+                Text("Choose theme")
+                ZStack {
+                    Capsule()
+                        .frame(width:80,height:44)
+                        .foregroundStyle(toggleOn ? .white : Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.6039008336)))
+                    ZStack{
+                        Circle()
+                            .frame(width:40, height:40)
+                            .foregroundStyle(toggleOn ? .black : .white)
+                        Image(systemName: toggleOn ? "moon.fill" : "sun.min")
+                    }
+
+                    .shadow(color: .black.opacity(0.14), radius: 4, x: 0, y: 2)
+                    .offset(x:toggleOn ? 18 : -18)
+                    .padding(24)
+                    //.animation(.spring(), value: toggleOn)
+                }
+                .onTapGesture {
+                    self.toggleOn.toggle()
+                    isDarkMode.toggle()
+                }
+            }
             Spacer()
         }
     }
