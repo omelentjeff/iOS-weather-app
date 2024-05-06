76665ab3813800787ed804aae6d466667c0927b0
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-06 12:23:10 +0300

    feat: create HomeView template

---
+++ WeatherApp/WeatherApp/Views/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 6.5.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
