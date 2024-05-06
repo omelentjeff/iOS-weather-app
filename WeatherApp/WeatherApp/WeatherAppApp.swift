0db55fbf351766877c809b1d0a04a25b695c7529
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-06 18:14:14 +0300

    feat: implement dark mode

--- WeatherApp/WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct WeatherAppApp: App {
+    
+    @AppStorage("isDarkMode") var isDarkMode: Bool = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
