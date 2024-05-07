3a8bd30559e63b5623fc1f596bac491da49223be
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-07 13:52:07 +0300

    feat: add background color to title and move to the upper left corner

--- WeatherApp/WeatherApp/Views/TitleView.swift
+++ WeatherApp/WeatherApp/Views/TitleView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct TitleView: View {
     var body: some View {
         HStack {
-            Image(systemName: "sun.horizon.fill").font(.system(size: 28)).padding(.trailing, 5).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            Text("Weather App").font(.system(size: 24))
-        }.padding()
+            Image(systemName: "sun.horizon.fill").font(.system(size: 20)).padding(.trailing, 5).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Text("Weather App").font(.system(size: 20))
+            Spacer()
+        }.padding(.bottom, 10).padding(.leading, 10).background(.blue.opacity(0.2))
     }
 }
