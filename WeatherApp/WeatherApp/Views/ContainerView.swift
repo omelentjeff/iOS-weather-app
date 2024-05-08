34074a73c334bcb1ce767ccc7f99a6961678a906
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-08 17:16:17 +0300

    feat: create ContainerView template

---
+++ WeatherApp/WeatherApp/Views/ContainerView.swift
@@ -0,0 +1,14 @@
+//
+//  ContainerView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 8.5.2024.
+//
+
+import SwiftUI
+
+struct ContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ WeatherApp/WeatherApp/Views/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 8.5.2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
