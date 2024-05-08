5ddb0f636fc422c1558f5b82fa97fce9cc3ff5a0
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-08 18:35:38 +0300

    feat: implement splash screen display logic

--- WeatherApp/WeatherApp/Views/ContainerView.swift
+++ WeatherApp/WeatherApp/Views/ContainerView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct ContainerView: View {
+    @State private var isSplashScreenViewPresented = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if !isSplashScreenViewPresented {
+            ContentView()
+        } else {
+            SplashScreenView(isPresented: $isSplashScreenViewPresented)
+        }
     }
 }
