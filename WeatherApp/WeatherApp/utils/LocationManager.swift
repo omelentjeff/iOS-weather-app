5e9a10ae9216a04dfedf34ddb1ba8fe5df9edf80
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-09 14:26:29 +0300

    feat: create LocationManager to handle current location

---
+++ WeatherApp/WeatherApp/utils/LocationManager.swift
@@ -0,0 +1,29 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 9.5.2024.
+//
+
+import Foundation
+import CoreLocation
+import CoreLocationUI
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+
+    @Published var location: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestLocation() {
+        manager.requestLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+    }
+}
