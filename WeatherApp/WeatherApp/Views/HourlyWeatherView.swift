ac78d0f088ec364bbb3479305ecab1bcf974632e
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-10 21:16:00 +0300

    feat: create HourlyWeatherView template

---
+++ WeatherApp/WeatherApp/Views/HourlyWeatherView.swift
@@ -0,0 +1,14 @@
+//
+//  HourlyWeatherView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 10.5.2024.
+//
+
+import SwiftUI
+
+struct HourlyWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
