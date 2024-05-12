9c1bc645a9362ca3576becc197683335c2872016
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-12 12:45:37 +0300

    feat: make home page scrollable and indicator off

--- WeatherApp/WeatherApp/Views/HomeView.swift
+++ WeatherApp/WeatherApp/Views/HomeView.swift
@@ -12,14 +12,23 @@ struct HomeView: View {
     @StateObject private var viewModel = LocationViewModel()
     
     var body: some View {
-        VStack {
-            CurrentWeatherView(coordinates: viewModel.coordinates)
-            
-            Spacer()
-            
-            //SevenDayForecastView()
-        }.onAppear {
-            viewModel.checkIfLocationServicesIsEnabled()
+        ZStack {
+            Rectangle().foregroundStyle(.blue.opacity(0.2))
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    CurrentWeatherView(coordinates: viewModel.coordinates)
+                    
+                    //Spacer()
+                    
+                    HourlyWeatherView()
+                    
+                    // Spacer()
+                    
+                    SevenDayForecastView()
+                }.onAppear {
+                    viewModel.checkIfLocationServicesIsEnabled()
+                }.padding(.bottom, 10)
+            }
         }
     }
 }
