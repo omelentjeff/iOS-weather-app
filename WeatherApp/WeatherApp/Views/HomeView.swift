eb4da3092228af42b7f354f6f2fb0e04f653ef61
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-12 19:13:13 +0300

    feat: implement horizontal scrolling across multiple weather location screens

--- WeatherApp/WeatherApp/Views/HomeView.swift
+++ WeatherApp/WeatherApp/Views/HomeView.swift
@@ -14,17 +14,11 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Rectangle().foregroundStyle(.blue.opacity(0.2))
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    CurrentWeatherView(coordinates: viewModel.coordinates)
-                    
-                    //Spacer()
-                    
-                    HourlyWeatherView().padding(.bottom, 20)
-                    
-                    // Spacer()
-                    
-                    SevenDayForecastView()
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(1...4, id: \.self) { _ in
+                        WeatherContainer(coordinates: viewModel.coordinates).containerRelativeFrame(.horizontal, count: 1, spacing: 10)
+                    }
                 }.onAppear {
                     viewModel.checkIfLocationServicesIsEnabled()
                 }.padding(.bottom, 10)
