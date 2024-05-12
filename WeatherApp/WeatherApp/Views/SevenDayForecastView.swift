316099bdb3caa9c34ac4bef790901aaf373b39e8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-12 12:48:03 +0300

    feat: add styling

--- WeatherApp/WeatherApp/Views/SevenDayForecastView.swift
+++ WeatherApp/WeatherApp/Views/SevenDayForecastView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct SevenDayForecastView: View {
     var body: some View {
-        GroupBox(label:
-                    Label("7 Day Forecast", systemImage: "sune").foregroundStyle(.blue)
-        ) {
-            HStack {
-                Spacer()
-                Image(systemName: "sun.max.fill").font(.system(size: 42)).foregroundStyle( Color.yellow)
-                Spacer()
-                Text("28°").font(.system(size: 36))
-                Spacer()
-            }.padding()
-        }.padding()
+        ScrollView {
+            GroupBox("7 Days") {
+                ForEach(1...20, id: \.self) { _ in
+                    Divider()
+                    DailyItemView()
+                }
+            }.backgroundStyle(.blue.opacity(0.3)).frame(width: 340)
+        }
     }
 }
