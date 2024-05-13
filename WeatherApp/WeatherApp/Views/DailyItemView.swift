439671ddd82fada732416bd3afca8ca58ede6f22
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-13 19:58:52 +0300

    feat: implement animated chevron.up icon to indicate button action

--- WeatherApp/WeatherApp/Views/DailyItemView.swift
+++ WeatherApp/WeatherApp/Views/DailyItemView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct DailyItemView: View {
     let isSelected: Bool
+    let buttonAction: () -> Void
+    @State private var rotationAngle = Angle(degrees: 180)
     
     var body: some View {
         HStack {
-            Text("la").padding(.trailing, 20)
+            Text("la")
             Image(systemName: "sun.max.fill").foregroundStyle(Color.yellow).padding(.bottom, 5)
             Spacer()
             Text("9°")
+            Button(action: {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    self.buttonAction()
+                    rotationAngle += isSelected ? Angle(degrees: -180) : Angle(degrees: 180)
+                }
+            }) {
+               Image(systemName: "chevron.up").rotationEffect(rotationAngle)
+            }.padding(.leading, 30)
         }.padding()
     }
 }
--- WeatherApp/WeatherApp/Views/SevenDayForecastView.swift
+++ WeatherApp/WeatherApp/Views/SevenDayForecastView.swift
@@ -9,17 +9,27 @@ import SwiftUI
 
 struct SevenDayForecastView: View {
     @State private var selectedDailyIndex: Int? = nil
+    @State private var isHourlyViewVisible: Bool = false
+    
     var body: some View {
         ScrollView {
             GroupBox("7 Days") {
                 ForEach(1...20, id: \.self) { index in
                     Divider()
-                    DailyItemView(isSelected: selectedDailyIndex == index).onTapGesture {
-                        selectedDailyIndex = selectedDailyIndex == index ? nil : index
-                    }
+                    DailyItemView(isSelected: selectedDailyIndex == index, buttonAction: {
+                        if self.selectedDailyIndex == index {
+                                self.selectedDailyIndex = nil
+                                self.isHourlyViewVisible = false
+                            } else {
+                                self.selectedDailyIndex = index
+                                self.isHourlyViewVisible = true
+                            }
+                    })
                     
                     if selectedDailyIndex == index {
-                        HourlyWeatherView()
+                        if isHourlyViewVisible {
+                            HourlyWeatherView()
+                        }
                     }
                 }
             }.backgroundStyle(.blue.opacity(0.3)).frame(width: 340)
