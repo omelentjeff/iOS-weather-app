6b8cf1336bea1943fcf4e2a6cf4f4bacc52b3072
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-16 19:37:51 +0300

    feat: display a map

--- WeatherApp/WeatherApp/Views/MapView.swift
+++ WeatherApp/WeatherApp/Views/MapView.swift
@@ -10,10 +10,6 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map().ignoresSafeArea()
     }
-}
-
-#Preview {
-    MapView()
 }
