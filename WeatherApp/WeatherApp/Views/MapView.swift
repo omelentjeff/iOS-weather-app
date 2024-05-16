0f8460047e4c4d1cb5230de5cb4eed963db91957
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-16 19:33:40 +0300

    feat: create MapView and import MapKit

---
+++ WeatherApp/WeatherApp/Views/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 16.5.2024.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
