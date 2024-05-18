6276509bef3b41c9407b8f7f9f9bf10490b01fe6
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-18 15:39:18 +0300

    feat: add current date to home screen

--- WeatherApp/WeatherApp/Views/CurrentWeatherView.swift
+++ WeatherApp/WeatherApp/Views/CurrentWeatherView.swift
@@ -14,7 +14,11 @@ struct CurrentWeatherView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Text(item.title).font(.system(size: 20)).bold()
+            HStack {
+                Text(item.title).font(.system(size: 20)).bold()
+                Spacer()
+                Text(getFormattedDate()).font(.system(size: 14))
+            }
             HStack {
                 Text(item.temp).font(.system(size: 56)).fontWeight(.light).padding(.trailing, 5)
                 Image(systemName: item.icon).font(.system(size: 40)).foregroundStyle(.yellow)
@@ -23,3 +27,9 @@ struct CurrentWeatherView: View {
         }.padding()
     }
 }
+
+func getFormattedDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.string(from: Date())
+    }
