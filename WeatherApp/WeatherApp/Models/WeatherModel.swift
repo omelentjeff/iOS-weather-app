efd57c5162e4f26a917fae9f5c29c26ece3a1ed4
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-22 20:09:06 +0300

    feat: implement basic WeatherModel

--- WeatherApp/WeatherApp/Models/WeatherModel.swift
+++ WeatherApp/WeatherApp/Models/WeatherModel.swift
@@ -5,4 +5,20 @@
 //  Created by Otto Melentjeff on 22.5.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherData: Codable {
+    let latitude: Double
+    let longitude: Double
+    let current: Current
+}
+
+struct Current: Codable {
+    let temperature2M: Double
+}
+
+enum WeatherError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
