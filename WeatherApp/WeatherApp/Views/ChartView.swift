ed20a626201df86f1bba26d1d6950570a916b801
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-30 14:43:20 +0300

    feat: implement fully working chart for rain probability

---
+++ WeatherApp/WeatherApp/Views/ChartView.swift
@@ -0,0 +1,31 @@
+//
+//  ChartView.swift
+//  WeatherApp
+//
+//  Created by Otto Melentjeff on 30.5.2024.
+//
+
+import SwiftUI
+import Charts
+
+struct ChartView: View {
+    @ObservedObject var viewModel: WeatherViewModel
+    @State private var rainData: [RainData] = []
+
+    var body: some View {
+        VStack {
+            Text("Rain %").font(.title)
+            Chart {
+                ForEach(rainData) { item in
+                    LineMark(
+                        x: PlottableValue.value("Date", item.date),
+                        y: PlottableValue.value("%", item.rainProbability)
+                    )
+                }
+            }
+        }
+        .onAppear {
+            self.rainData = self.viewModel.getSevenDaysRainProbabilities()
+        }
+    }
+}
