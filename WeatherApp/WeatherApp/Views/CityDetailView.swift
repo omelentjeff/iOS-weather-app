c72d6988723d02c61db7baf150905d2bc79301cc
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-30 12:18:11 +0300

    feat: background color amtches parent and custom back button

--- WeatherApp/WeatherApp/Views/CityDetailView.swift
+++ WeatherApp/WeatherApp/Views/CityDetailView.swift
@@ -11,15 +11,37 @@ import CoreLocation
 struct CityDetailView: View {
     var city: SearchResult
     @ObservedObject var viewModel: WeatherViewModel
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     
     var body: some View {
-        HStack {
-            HomeView(viewModel: viewModel, coordinates: CLLocationCoordinate2D(latitude: city.latitude, longitude: city.longitude), title: city.name)
-        }.onAppear {
-            DispatchQueue.main.async {
-                viewModel.fetchWeather(for: Date(), coordinates: CLLocationCoordinate2D(latitude: city.latitude, longitude: city.longitude))
+            ZStack(alignment: .topLeading) {
+                VStack {
+                    HomeView(viewModel: viewModel, coordinates: CLLocationCoordinate2D(latitude: city.latitude, longitude: city.longitude), title: city.name)
+                }
+                .onAppear {
+                    DispatchQueue.main.async {
+                        viewModel.fetchWeather(for: Date(), coordinates: CLLocationCoordinate2D(latitude: city.latitude, longitude: city.longitude))
+                    }
+                }
+                .navigationTitle("")
+                .toolbar(.hidden, for: .navigationBar)
+                
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    HStack {
+                        Image(systemName: "chevron.left").font(.system(size: 18))
+                        Text("Back").font(.system(size: 18))
+                    }
+                    .tint(Color.white)
+                    //.padding(.bottom, 40)
+                    //.background(Color.white.opacity(0.8))
+                    //.cornerRadius(8)
+                }
+                .padding()
             }
-        }.navigationBarHidden(true)
-    }
+            //.background(Color.blue.opacity(0.2))
+            
+        }
 }
--- WeatherApp/WeatherApp/Views/SearchView.swift
+++ WeatherApp/WeatherApp/Views/SearchView.swift
@@ -11,6 +11,15 @@ struct SearchView: View {
     @ObservedObject var searchViewModel: SearchViewModel
     @ObservedObject var weatherViewModel: WeatherViewModel
     
+    /*
+    init() {
+        // Set the navigation bar appearance to be hidden, but keep the back button visible
+        let appearance = UINavigationBar.appearance()
+        appearance.setBackgroundImage(UIImage(), for: .default)
+        appearance.shadowImage = UIImage()
+        appearance.isTranslucent = true
+    }*/
+    
     /*init() {
       UITextField.appearance().clearButtonMode = .whileEditing
     }*/
@@ -20,34 +29,46 @@ struct SearchView: View {
                VStack {
                    HStack {
                        Spacer()
-                       Image(systemName: "magnifyingglass")
+                       Image(systemName: "magnifyingglass").font(.system(size: 18))
                        TextField("Search for a city...", text: $searchViewModel.currentValue)
                            .onChange(of: searchViewModel.debouncedValue) { newValue in
                                searchViewModel.fetchSearchResults(for: newValue)
                            }
                            .textFieldStyle(OvalTextFieldStyle())
                            .padding()
-                   }
+                   }.padding(.horizontal, 10)
+                   Divider()
                    
-                   Spacer()
+                   //Spacer()
                    
-                   if let searchData = searchViewModel.searchData?.results, !searchData.isEmpty {
-                       List(searchData, id: \.id) { city in
-                           NavigationLink(destination: CityDetailView(city: city, viewModel: weatherViewModel)/*toolbar(
-                            .hidden, for: .navigationBar)*/) {
-                               Text("\(city.name), \(city.country)")
+                   if searchViewModel.loading {
+                           ProgressView()
+                               .padding()
+                       } else {
+                           // Check for search results
+                           if let searchData = searchViewModel.searchData?.results, !searchData.isEmpty {
+                               List(searchData, id: \.id) { city in
+                                   NavigationLink(destination: CityDetailView(city: city, viewModel: weatherViewModel)) {
+                                       Text("\(city.name), \(city.country)")
+                                   }
+                                   .padding()
+                                   .listRowBackground(Color.clear)
+                               }
+                               .listStyle(PlainListStyle())
+                           } else {
+                               // Display "No results found" message if there are no search results
+                               Text(searchViewModel.debouncedValue.isEmpty ? "" : "No results found")
+                                   .padding()
                            }
                        }
-                   } else {
-                       Text(searchViewModel.debouncedValue.isEmpty ? "" : "No results found")
-                           .padding()
-                   }
                    Spacer()
                }
+               .scrollContentBackground(.hidden)
                .background(Color.blue.opacity(0.2))
-               .navigationTitle("Search")
-               .navigationBarBackButtonHidden(false)
-           }
+               .navigationBarHidden(true)
+               .edgesIgnoringSafeArea(.top)
+           }//.background(Color.blue.opacity(0.2))
+           //.ignoresSafeArea()
        }
 }
 
