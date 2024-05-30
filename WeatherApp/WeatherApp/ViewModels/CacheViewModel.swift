d04111ea5416be4f3e6af05b78e7db13cdc63ca8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-05-30 19:14:39 +0300

    feat: implement cache functions

--- WeatherApp/WeatherApp/ViewModels/CacheViewModel.swift
+++ WeatherApp/WeatherApp/ViewModels/CacheViewModel.swift
@@ -8,5 +8,21 @@
 import SwiftUI
 
 final class CacheViewModel: ObservableObject {
+    @Published var cachedData: WeatherData?
+    
     let manager = CacheManager.instance
+    
+    func saveToCache(data: WeatherData, id: Int) {
+        let wrappedData = StructWrapper(data)
+        manager.add(data: wrappedData, id: id)
+    }
+    
+    func removeFromCache(id: Int) {
+        manager.remove(id: id)
+    }
+    
+    func getFromCache(id: Int) {
+        let wrappedData = manager.get(id: id)
+        cachedData = wrappedData?.value
+    }
 }
